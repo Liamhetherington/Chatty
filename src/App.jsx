676749c22bd6efedca7b4966821f2d7508dcc10f
Liam Hetherington <liambhetherington@gmail.com>
2019-06-04 18:52:03 +0000

    implemented JSC template and broke down HTML into seperate component files. kept static html

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import MessageList from "./MessageList.jsx";
+import ChatBar from "./ChatBar.jsx";
+
+const data = {
+  currentUser: {
+    name: "Bob"
+  }, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?"
+    },
+    {
+      username: "Anonymous",
+      content:
+        "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: data.currentUser.name,
+      messages: []
+    };
+  }
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">
+            Chatty
+          </a>
+        </nav>
+        <MessageList />
+        <ChatBar currentUser={this.state.currentUser.name} />
+      </div>
     );
   }
 }
---
+++ src/ChatBar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class ChatBar extends Component {
+  render() {
+    return (
+      <footer className="chatbar">
+        <input className="chatbar-username" defaultValue={this.props.name} />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+        />
+      </footer>
+    );
+  }
+}
+
+export default ChatBar;
