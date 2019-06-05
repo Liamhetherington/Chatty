790590f21bc1dabebeed0cc8232064d386d10fd3
Liam Hetherington <liambhetherington@gmail.com>
2019-06-05 21:34:18 +0000

    create buildmessage helper app and change handler to send message over socket to console

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -19,6 +19,11 @@ const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
     console.log('Client connected');
 
+    ws.on('message', (message) => {
+        const post = JSON.parse(message).data;
+        console.log("User " + post.username + " said " + post.content);
+    })
+
     // Set up a callback for when a client closes the socket. This usually means they closed their browser.
     ws.on('close', () => console.log('Client disconnected'));
 });
